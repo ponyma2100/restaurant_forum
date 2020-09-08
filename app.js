52194e07a0fbfa61ec361f799736bc08225abdc7
Pony <ponyma2100@gmail.com>
2020-09-08 12:02:27 +0800

    feat: add admin restaurant index page

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express()
 const exphbs = require('express-handlebars')
 const port = 3000
 
-app.engine('exphbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.listen(port, () => {
-  console.log(`Express is listening on https://localhost:${port}`)
+  console.log(`Express is listening on http://localhost:${port}`)
 })
+
+require('./routes')(app)
