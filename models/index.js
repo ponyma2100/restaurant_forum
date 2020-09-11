829b2b6786697ca6afca5424d9006d5226067c60
Pony <ponyma2100@gmail.com>
2020-09-11 19:51:07 +0800

    modify index.js

--- models/index.js
+++ models/index.js
@@ -12,9 +12,8 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config.ssl = {
-    rejectUnauthorized: false
-  }, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  console.log(config)
 }
 
 fs
