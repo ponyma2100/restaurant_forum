1193e0c7b7f4e934097e7ec0b75dd3f13f57bdd8
Pony <ponyma2100@gmail.com>
2020-09-11 19:23:06 +0800

    modify index.js

--- models/index.js
+++ models/index.js
@@ -12,16 +12,9 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {
-    "use_env_variable": "DATABASE_URL",
-    "dialect": "postgres",
-    "protocol": "postgres",
-    "dialectOptions": {
-      "ssl": {
-        "rejectUnauthorized": false
-      }
-    }
-  });
+  sequelize = new Sequelize(config.database, config.username, config.password, config.ssl = {
+    rejectUnauthorized: false
+  }, config);
 }
 
 fs
