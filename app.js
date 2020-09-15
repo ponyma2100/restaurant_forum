86abb44e89b7294739e00540f851555dad9e9bcb
Pony <ponyma2100@gmail.com>
2020-09-16 02:08:33 +0800

    feat: add category select in edit page using ifCond-helper

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 app.use('/upload', express.static(__dirname + '/upload'))
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: require('./config/handlebars-helpers') }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({
