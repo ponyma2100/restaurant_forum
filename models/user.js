fb039346f5e63906bbfc1291b0d6d1d155b5d811
Pony <ponyma2100@gmail.com>
2020-09-20 15:40:15 +0800

    feat: Create Followship model and add followers and followings to users

--- models/user.js
+++ models/user.js
@@ -22,6 +22,11 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'UserId',
         as: 'LikedRestaurants'
       })
+      User.belongsToMany(models.User, {
+        through: models.Followship,
+        foreignKey: 'FollowingId',
+        as: 'Followers'
+      })
     }
   };
   User.init({
