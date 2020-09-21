4d2e5c935dd6e4d5ccfd3ba89eb2cce667bafc1e
Pony <ponyma2100@gmail.com>
2020-09-21 23:48:48 +0800

    refactor categoryController.getCategories

---
+++ controllers/api/categoryController.js
@@ -0,0 +1,15 @@
+const db = require('../../models/')
+const Category = db.Category
+const categoryService = require('../../services/categoryService')
+
+const categoryController = {
+  // 瀏覽分類
+  getCategories: (req, res) => {
+    categoryService.getCategories(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+
+}
+
+module.exports = categoryController
