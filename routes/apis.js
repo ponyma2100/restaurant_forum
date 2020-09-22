47478b84bca195fc16575f0146136975beb1bc93
Pony <ponyma2100@gmail.com>
2020-09-23 01:54:39 +0800

    POST /api/signin (return jwt)

--- routes/apis.js
+++ routes/apis.js
@@ -6,6 +6,8 @@ const upload = multer({ dest: 'temp/' })
 
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
+const userController = require('../controllers/api/userController')
+
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
@@ -16,5 +18,7 @@ router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
 router.get('/admin/categories', categoryController.getCategories)
 router.get('/admin/categories/:id', categoryController.getCategories)
+
+router.post('/signin', userController.signIn)
 
 module.exports = router
