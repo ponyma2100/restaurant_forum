2c0c1f0b08fa3e9af9c0c39253ff222adbf60577
Pony <ponyma2100@gmail.com>
2020-09-23 22:03:00 +0800

    refactor adminController.putRestaurant

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -22,6 +22,12 @@ const adminController = {
     })
   },
 
+  putRestaurant: (req, res) => {
+    adminService.putRestaurant(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+
   deleteRestaurant: (req, res) => {
     adminService.deleteRestaurant(req, res, (data) => {
       return res.json(data)
